31ed4b4b97c5d15c35fb138e6644cb950540a05a
haikelz <haikel.2017th@gmail.com>
2022-07-25 10:27:54 +0700

    fix: remove unused tag css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   mode: "jit",
-  darkMode: "class", 
+  darkMode: "class",
   theme: {
-    extend: {
-      boxShadow: {
-        cards: "10px 10px 0px -3px rgba(0,0,0,1)",
-        dark: "10px 10px 0px -3px white", 
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
