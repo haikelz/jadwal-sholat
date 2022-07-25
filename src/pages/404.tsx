f625300a390299430d28ff8e6d4f3d6727161a25
haikelz <haikel.2017th@gmail.com>
2022-07-25 18:00:32 +0700

    feat: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Image from "next/image";
+
+const ErrorPage = () => {
+  const Router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      Router.push("/");
+    }, 5000);
+  }, [Router]);
+
+  return (
+    <div className="flex justify-center text-center min-h-screen items-center flex-col">
+      <Image src="/img/404.png" width="150px" height="130px" alt="404 Image" />
+      <h1 className="font-bold text-xl">
+        Maaf, halaman yang anda tuju tidak ditemukan
+      </h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
