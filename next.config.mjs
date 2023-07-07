0da8c00427bc2214c39f97d42111c3daeb574a39
haikelz <haikel.2017th@gmail.com>
2023-07-07 21:03:04 +0700

    fix: remove unnecessary comments

--- next.config.mjs
+++ next.config.mjs
@@ -19,24 +19,3 @@ export default withPWA({
     return config;
   },
 });
-// import "./src/env.mjs";
-
-/*const WindiCSSWebpackPlugin = require("windicss-webpack-plugin");
-
-/** @type {import('next').NextConfig} 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
-module.exports = withPWA({
-  reactStrictMode: true,
-  swcMinify: true,
-  compress: true,
-  webpack(config) {
-    config.plugins.push(new WindiCSSWebpackPlugin());
-    return config;
-  },
-});*/
