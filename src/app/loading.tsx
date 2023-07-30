e6e24700a49cf0ad02941227cbb6c55b6b2deadc
haikelz <haikel.2017th@gmail.com>
2023-07-30 18:57:56 +0700

    chore: prepare to merge this to master

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,9 +2,7 @@ import { cx } from "classix";
 
 export default function Loading() {
   return (
-    <div
-      className={cx("flex min-h-screen items-center justify-center", "space-x-3 p-8 text-center")}
-    >
+    <div className={cx("flex min-h-screen items-center justify-center", "p-8 text-center")}>
       <p className="text-2xl font-bold">Loading</p>
       <div role="status">
         <svg
