f76d27c2fb3540a20f290766e6bf92faaf77b978
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2025-02-17 12:02:56 +0700

    hotfix: cypress

--- apps/jadwal-sholat/cypress/support/component.ts
+++ apps/jadwal-sholat/cypress/support/component.ts
@@ -15,7 +15,7 @@
 // Import commands.js using ES2015 syntax:
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-import { mount } from "cypress/react18";
+import { mount } from "cypress/react";
 
 import "../../src/app/globals.css";
 import "./commands";
